c9017c4ccdb230f1ae07b218bb1c9c11100e45b9
Martínez Comesaña <mmcomesana@minsait.com>
2024-03-01 19:48:49 +0100

    More modification to send emails

--- resources/user.py
+++ resources/user.py
@@ -24,7 +24,7 @@ def send_simple_message(to, subject, body):
     return requests.post(
 		f"https://api.mailgun.net/v3/{domain}/messages",
 		auth=("api", os.getenv("MAILGUN_API_KEY")),
-		data={"from": "Mike <mailgun@{domain}>",
+		data={"from": f"Mike <mailgun@{domain}>",
 			"to": [to],
 			"subject": subject,
 			"text": body})
